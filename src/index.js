332f8dfb600a9ceff119e4ec00e63179bd044438
John Heaven <john@johnheaven.eu>
2022-10-16 14:05:25 +0200

    a few changes...

--- src/index.js
+++ src/index.js
@@ -1,15 +1,33 @@
+/* React imports */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import axios from 'axios';
 import './index.css';
 
+/* React-Bootstrap stuff */
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Table from 'react-bootstrap/Table';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Stack from 'react-bootstrap/Stack';
+import Navbar from 'react-bootstrap/Navbar';
+import Alert from 'react-bootstrap/Alert';
 
+/* Reactchartjs stuff */
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend
+     } from 'chart.js';
+
+import { Bar } from 'react-chartjs-2';
+
+/* Main app */
 class DothDashboard extends React.Component {
     constructor(props) {
         super(props);
@@ -28,10 +46,17 @@ class DothDashboard extends React.Component {
         if (this.state.allData)
             return (
                 <>
-                <Container>
+                <Navbar bg="dark" variant="dark" expand="lg" sticky="top">
+                    <Container>
+                        <Stack gap={5}>
+                            <Navbar.Brand href="#home"><h1>Dog of the Hour</h1></Navbar.Brand>
+                        </Stack>
+                    </Container>
+                </Navbar>
+                <Container className="pt-3">
                     <Stack gap={5}>
                         <Row>
-                            <section class="current-dog">
+                            <section className="current-dog">
                                 <CurrentDog data={this.state.allData.current_dog} />
                             </section>
                         </Row>
@@ -41,13 +66,13 @@ class DothDashboard extends React.Component {
                             </section>
                         </Row>
                         <Row>
-                            <section class="monthly">
+                            <section className="monthly">
                                 <Monthly data={this.state.allData.monthly} />
                             </section>
                         </Row>
                         <Row>
-                            <section class="leaderboard">
-                                <RunsLeaderboard data={this.state.allData.runs_by_month.table} />
+                            <section className="leaderboard">
+                                <RunsLeaderboard data={this.state.allData.runs_by_month} />
                             </section>
                         </Row>
                     </Stack>
@@ -61,11 +86,49 @@ class DothDashboard extends React.Component {
 }
 
 function Monthly({data}) {
-    return (
-    <>
-    <BsTable tableData={data.table} title='Monthly Standings' />
-    </>
-    );
+    if (data) {
+        return (
+        <>
+        <BsTable tableData={data.table} title='Monthly Standings' />
+        <ChartJSBar data={data.chart} />
+        </>
+        );
+    }
+}
+
+function ChartJSBar({data}) {
+    if ({data}) {
+        const labels = [
+            'January',
+            'February',
+            'March',
+            'April',
+            'May',
+            'June',
+        ];
+        const chart_data = {
+            labels: labels,
+            datasets: [{
+                label: 'My First dataset',
+                backgroundColor: 'rgb(255, 99, 132)',
+                borderColor: 'rgb(255, 99, 132)',
+                data: [0, 10, 5, 2, 20, 30, 45],
+                }]
+        };
+        
+        ChartJS.register(
+            CategoryScale,
+            LinearScale,
+            BarElement,
+            Title,
+            Tooltip,
+            Legend
+        );
+
+        return(
+            <Bar data={data} />
+        );
+    };
 }
 
 function BsTable({tableData, title}) {
@@ -108,8 +171,10 @@ function CurrentDog({data}) {
     if (data) {
         return (
             <>
-                <h2>{data.dogname} is Dog of the Hour!</h2>
-                <span class="doth-current-hour">{data.date}</span>
+                <Row>
+                    <Alert variant="success">{data.dogname} is Dog of the Hour!</Alert>
+                    <span className="doth-current-hour">{data.date}</span>
+                </Row>
             </>
         );
     }
@@ -119,7 +184,7 @@ function CurrentDog({data}) {
 function TodaysDogs({data}) {
     if (data) {
         return (
-            <BsTable tableData={data.table} title={"Today's Dogs"} />
+            <BsTable tableData={data.table} title={"Today's Winners"} />
         )
     }
 }
@@ -127,7 +192,10 @@ function TodaysDogs({data}) {
 function RunsLeaderboard({data}) {
     if (data) {
         return (
-            <BsTable tableData={data} title={'Runs Leaderboard'} />
+            <>
+            <BsTable tableData={data.table} title={'Runs Leaderboard'} />
+            <ChartJSBar data={data.chart} />
+            </>
         )
     }
 }
